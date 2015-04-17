d0a5ae7e2ffe3da4f42c3692c515ed5a3f4e604a
Ekeyme Mo <ekeyme@gmail.com>
2015-04-18 00:13:57 +0800

    syntx correct

--- Qshutdown.bat
+++ Qshutdown.bat
@@ -10,7 +10,7 @@ set shutdown=C:\Windows\System32\shutdown.exe
 if %input% == 0 %shutdown% -s -t 0 & goto end
 if %input% == c %shutdown% -a & goto end
 
-set/a time=60*%input%*
+set/a time=60*%input%
 if %time% GTR 0 (%shutdown% -s -t %time%) else echo "Error: The time must be int value and bigger than 0."
 goto end
 

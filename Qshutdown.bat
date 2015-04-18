34b13f4da0bee4b17118a14046277236cbf49de1
Ekeyme Mo <ekeyme@gmail.com>
2015-04-18 13:15:08 +0800

    use para 00 to repalce 0 to shutdown Now
    add loop if user give the unaccepted value
    
    docstring modify

--- Qshutdown.bat
+++ Qshutdown.bat
@@ -1,18 +1,17 @@
 @echo off
-echo ---------------------------------------------------
-echo ":0 for shutdown NOW; :c for Cancel shutdown plan; :Number for min to shutdown"
+echo ------------------------------------------------------------------------------
+echo :00 for shutdown NOW; :c for Cancel shutdown plan; :Number for min to shutdown
+echo ------------------------------------------------------------------------------
 
+:start
 set/p input= >nul
-echo ---------------------------------------------------
-
 set shutdown=C:\Windows\System32\shutdown.exe
 
-if %input% == 0 %shutdown% -s -t 0 & goto end
+if %input% == 00 %shutdown% -s -t 0 & goto end
 if %input% == c %shutdown% -a & goto end
 
 set/a time=60*%input%
-if %time% GTR 0 (%shutdown% -s -t %time%) else echo "Error: The time must be int value and bigger than 0."
-goto end
+if %time% GTR 0 (%shutdown% -s -t %time%) else echo Error: The time must be int value and bigger than 0. Input again: & goto start
 
 :end
 Pause
